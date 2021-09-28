05b2c6a73c0053ec5625458e03dbdbaf8af334ce
Alexey Evtyushkin <ea42ru@mail.ru>
2021-09-28 11:25:27 +0300

    main

---
+++ src/ru/ea42/EaLogger/App.java
@@ -0,0 +1,6 @@
+package ru.ea42.EaLogger;
+
+public class App {
+    public static void Start() {
+    }
+}
--- src/ru/ea42/EaLogger/Main.java
+++ src/ru/ea42/EaLogger/Main.java
@@ -1,8 +1,8 @@
 package ru.ea42.EaLogger;
+import java.io.IOException;
 
 public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        App.Start();
     }
 }
